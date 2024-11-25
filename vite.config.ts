4e9fab4090900a296f9afdbaa298b68265312fab
Diego Rodriguze <diego@rodriguez-digital.de>
2024-11-25 20:08:08 +0100

    fix: update build configurations for proper MIME types

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
       }
     }
   },
